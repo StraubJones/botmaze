1b4b629e854eedfc4024c57d36027e523bf19981
Jon Wright <jonwright@Jons-MacBook-Pro.local>
2017-11-18 11:52:38 -0500

    sets up component state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      inputValue: 'Cheese'
+    }
+  }
 
   componentDidMount () {
     this.updateCanvas();
@@ -13,16 +18,21 @@ class App extends Component {
     ctx.fillRect(0,0, 100, 100);
   }
 
+  commandEntered (value) {
+    console.log(value)
+  }
+
   render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Botmaze</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Type in a command to move the sprite through the maze. <br />
+          <input className="App-input" value="" type="text" placeholder="Enter a command"></input>
         </p>
+        
         <canvas ref="canvas" width={800} height={500}/>
       </div>
     );
